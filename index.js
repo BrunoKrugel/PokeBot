bc94e3d2630904a3b0da0b3ed1fe442213480e83
Bruno Krugel <30608179+BrunoKrugel@users.noreply.github.com>
2021-11-23 11:34:34 -0300

    Update discord v13
    Add env environment

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 //Setup
-const Discord = require("discord.js");
-const config = require('./config.json');
+const {
+    Client,
+    Intents,
+    MessageEmbed
+} = require('discord.js');
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
+
 const pokemon = require('./pokemon.json');
-const client = new Discord.Client();
-const Sequelize = require('sequelize');
-const fs = require('fs');
 
 //Save
 const Keyv = require('keyv');
 const pogo = new Keyv(); // for in-memory storage
 
+const dotenv = require('dotenv');
+dotenv.config();
 
-const sequelize = new Sequelize('database', 'user', 'password', {
-    host: 'localhost',
-    dialect: 'sqlite',
-    logging: false,
-    // SQLite only
-    storage: 'database.sqlite',
-});
-
-//Roubei do muller fodasi
 const peixes = [
     'https://i.imgur.com/8q8tCpJ.png',
     'https://i.imgur.com/t7xxQvM.png',
@@ -55,11 +52,12 @@ const peixes = [
     'https://i.imgur.com/jadIWJy.png',
 ];
 
-const ofensas = [", tem dois cu",
+const ofensas = [
+    ", tem dois cu",
     " tem pau pequeno",
     " é corno",
     " vai ti fude",
-    "queima a rosca",
+    " queima a rosca",
     " enfia no cu",
     " tem tourette",
     " comeu os cu do Zielort",
@@ -69,7 +67,12 @@ const ofensas = [", tem dois cu",
     " é chifrudo",
     " guampudo do caralho",
     " queimou carne de peixe e botou a culpa no bk",
-    " fez o Zielort quitar do discord"
+    " fez o Zielort quitar do discord",
+    " ficou com pressão baixa no oral",
+    " reza 3 pau nosso",
+    " joga new world",
+    " joga de yumi externa",
+    " goza com pau dos outros"
 ];
 
 //Pokedex
@@ -78,7 +81,7 @@ var pokedex = require('./pokedex.json');
 
 //Logs
 client.once('ready', () => {
-    console.log("Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.");
+    console.log("Bot has started.");
     client.user.setActivity("Pokemon");
     console.log(pokedex.pokemon.abra);
 });
@@ -86,24 +89,25 @@ client.once('reconnecting', () => {
     console.log('Reconnecting!');
 });
 client.once('disconnect', () => {
-    console.log('Disconnect!');
+    console.log('Disconnected!');
 });
 
 client.on("guildMemberAdd", (member) => {
-    member.guild.defaultChannel.send('${member.user} has joined this server');
+    member.guild.defaultChannel.send('${member.user} chegou.');
 });
 
 //Evento de mensagens
-client.on('message', async message => {
+client.on('messageCreate', async message => {
 
     // Se não for o proprio bot
     if (message.author.bot) return;
+
     //@all
-    if (!message.content.startsWith(config.prefix) && message.mentions.everyone && !message.author.bot) return message.channel.send('Ta marcando pq, corno?');
+    if (!message.content.startsWith(process.env.prefix) && message.mentions.everyone && !message.author.bot) return message.channel.send('Ta marcando pq é corno?');
 
     // Mensagem customizada
     var mens = Math.floor((Math.random() * 20) + 1);
-    if (parseInt(mens) == parseInt(5)) {
+    if (parseInt(mens) < parseInt(10)) {
         message.channel.send(message.author.toString() + " " + ofensas[Math.floor(Math.random() * ofensas.length)]);
     }
 
@@ -112,64 +116,61 @@ client.on('message', async message => {
     var poke = Math.floor((Math.random() * 10) + 1);
     console.log('Inicial: ' + await pogo.get('poke'));
     var pokequest = await pogo.get('poke');
-    //Se não existe pokemon escolhido
-    if (parseInt(poke) > parseInt(1) && ((pokequest == '0') || typeof pokequest == "undefined")) {
+
+    //Se não existe pokemon escolhido e 30% de chance de spawnar
+    if (parseInt(poke) > parseInt(7) && ((pokequest == '0') || typeof pokequest == "undefined")) {
         var imagem = Math.floor((Math.random() * 151) + 1);
-        console.log('Pokedex: ' + pokemon[imagem-1].toLowerCase());
-        const exampleEmbed = new Discord.MessageEmbed()
+        console.log('Pokedex: ' + pokemon[imagem - 1].toLowerCase());
+
+        const exampleEmbed = new MessageEmbed()
             .setTitle('Quem é esse pokemon?')
             .setImage('https://raw.githubusercontent.com/BrunoKrugel/PokemonImages/master/1GEN/' + imagem + '.png');
-        message.channel.send(exampleEmbed);
+        message.channel.send({
+            embeds: [exampleEmbed]
+        });
         // Salva o número do pokemon
         await pogo.set('poke', imagem);
         console.log('Novo: ' + await pogo.get('poke'));
     }
     var question = await pogo.get('poke', imagem);
     if ((question != 0) && (typeof question !== "undefined")) {
-        console.log('entrou pq: ' + question + ' e ' + pokemon[question-1].toLowerCase());
+        console.log('Entrou pois: ' + question + ' e ' + pokemon[question - 1].toLowerCase());
         console.log(message.content.toLowerCase());
-        if (message.content.toLowerCase() == pokemon[question-1].toLowerCase()) {
+        if (message.content.toLowerCase() == pokemon[question - 1].toLowerCase()) {
             await pogo.set('poke', 0);
-            message.channel.send(message.author.toString() + 'acertou');
+            message.channel.send(message.author.toString() + ' acertou');
         }
         console.log('Depois: ' + await pogo.get('poke'));
     };
 
     //========================== Ignora o proprio bot e mensagens q n sejam para o bot =======================
-    if (!message.content.toLowerCase().startsWith(config.prefix)) return;
+    if (!message.content.toLowerCase().startsWith(process.env.prefix)) return;
 
     //Salva os parametros em ARGS    
-    const args = message.content.slice(prefix.length).split(' ');
-    const command = args.shift().toLowerCase();
+    const args = message.content.split(' ');
 
-    if (message.content.toLowerCase().startsWith(config.prefix)) {
-        let param = args[0];
-        let seco = args[1];
+    if (message.content.toLowerCase().startsWith(process.env.prefix)) {
+        let primary = args[1];
+        let secondary = args[2];
         //=================
-        switch (param) {
+        switch (primary) {
             case "catch":
                 //https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png change number
                 break;
                 //============== Fun Section
             case "say":
-                // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-                // To get the "message" itself we join the `args` back into a string with spaces: 
                 const sayMessage = args.join(" ");
-                // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
                 message.delete().catch(O_o => {});
-                // And we get the bot to say the thing: 
-                message.channel.send(sayMessage.slice(3));
+                // 9 = "poke say "
+                message.channel.send(sayMessage.slice(9));
                 break;
             case "peixe":
-                //message.channel.send({ files: [peixes[Math.floor(Math.random() * peixes.length)].toString()] });
+                const exampleEmbed = new MessageEmbed()
+                    .setImage(peixes[Math.floor(Math.random() * peixes.length)].toString());
                 message.channel.send({
-                    embed: {
-                        color: 3447003,
-                        image: {
-                            url: peixes[Math.floor(Math.random() * peixes.length)].toString(),
-                        },
-                    }
+                    embeds: [exampleEmbed]
                 });
+
                 break;
             case "help":
                 break;
@@ -178,4 +179,4 @@ client.on('message', async message => {
         }
     };
 });
-client.login(config.token);
+client.login(process.env.token);
