3a8dccf5b7c794421bfbe5a2a23dfe27d137cee1
Bruno Krugel <brunokrugel@gmail.com>
2022-02-23 01:06:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -63,6 +63,8 @@ client.on('messageCreate', async message => {
         if (message.content.toLowerCase() == pokemon[question - 1].toLowerCase()) {
             await pogo.set('poke', 0);
             message.channel.send(message.author.toString() + ' acertou');
+
+            console.log(message.author.toString());
         }
         console.log('Depois: ' + await pogo.get('poke'));
     }
