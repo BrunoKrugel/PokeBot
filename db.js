7452912ef407ab750ddb179ec0bb9539a8c42e74
brunokrugel <brunokrugel@gmail.com>
2022-04-10 20:38:07 +0044

    Remove unnused material

--- db.js
+++ db.js
@@ -1,4 +1,3 @@
-var MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
 
 const Score = mongoose.model('Score', mongoose.Schema({
@@ -6,14 +5,8 @@ const Score = mongoose.model('Score', mongoose.Schema({
   score: Number
 }));
 
-const URL = "mongodb://localhost:27017/";
-const MONGO_DB = "PokeBot";
-const DB_TABLE = "Score";
-
-
 const dotenv = require('dotenv');
 dotenv.config();
-const uri = process.env.MONGODB_URI;
 
 async function connectDB(){
   try {
@@ -30,7 +23,6 @@ async function updateScores(name){
 }
 module.exports.updateScores=updateScores;
 
-
 function getScores(name){
   return Score.findOne({ name: name }).exec();
 }
@@ -39,50 +31,6 @@ module.exports.getScores=getScores;
 function getAllScores(){
   return Score.find({}).exec();
 }
-
-
-function updateScore(user) {
-  MongoClient.connect(URL, function (err, client) {
-    if (err) throw err;
-    var database = client.db(MONGO_DB);
-    var myquery = { id: user };
-    var newvalues = { $inc: { score: 1 } };
-    database.collection(DB_TABLE).updateOne(myquery, newvalues, { upsert: true }, function (err, result) {
-
-      if (err) throw err;
-      client.close();
-    });
-  });
-}
-module.exports.updateScore=updateScore;
-
-function getScore(user) {
-  MongoClient.connect(URL, function (err, client) {
-    if (err) throw err;
-    var database = client.db(MONGO_DB);
-    return database.collection(DB_TABLE).findOne({ id: user }, function (err, result) {
-      if (err) throw err;
-      //console.log(result.score)
-      client.close();
-    });
-
-  });
-}
-module.exports.getScore=getScore;
-
-async function getRanking() {
-  MongoClient.connect(URL, function (err, client) {
-    if (err) throw err;
-    var database = client.db(MONGO_DB);
-        database.collection(DB_TABLE).find({}, { projection: { _id: 0, id: 1, score: 1 } }).sort({ score: -1 }).limit(5).toArray(function (err, result) {
-        if (err) throw err;
-        client.close();
-        return result;
-      });
-    })
-
-}
-module.exports.getRanking=getRanking;
 
 connectDB();
 updateScores('oguilher');
